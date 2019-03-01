6cb27e13290b76b60477d29b744d3cb165615f04
John <persekiana@gmail.com>
2019-03-01 01:45:03 -0800

    edit

---
+++ split_dataset.py
@@ -0,0 +1,43 @@
+import os
+import imageio
+import shutil
+import imageio
+
+if __name__ == "__main__":
+    root='UCF11_updated_mpg'
+    classes=sorted(os.listdir(root))
+
+    path = 'UCF11_split'
+    train_path = path+'/train'
+    test_path = path+'/test'
+    if not os.path.exists(path):
+        os.makedirs(path)
+    if not os.path.exists(train_path):
+        os.makedirs(train_path)
+    if not os.path.exists(test_path):
+        os.makedirs(test_path)
+
+    for c in classes:
+        subclasses = sorted(os.listdir(root+'/'+c))
+        n=len(subclasses)-2
+        if not os.path.exists(train_path+'/'+c):
+            os.makedirs(train_path+'/'+c)
+        if not os.path.exists(test_path+'/'+c):
+            os.makedirs(test_path+'/'+c)
+
+        for i,subclass in enumerate(subclasses):
+            if subclass=='.DS_Store' or subclass=='Annotation':
+                continue
+            if i<(0.8*n):
+                videos = sorted(os.listdir(root+'/'+c+'/'+subclass))
+                for video in videos: 
+                    src_dir = root+'/'+c+'/'+subclass+'/'+video
+                    dst_dir = train_path+'/'+c+'/'+video
+                    shutil.copy(src_dir,dst_dir)
+            else:
+                videos = sorted(os.listdir(root+'/'+c+'/'+subclass))
+                for video in videos: 
+                    src_dir = root+'/'+c+'/'+subclass+'/'+video
+                    dst_dir = test_path+'/'+c+'/'+video
+                    shutil.copy(src_dir,dst_dir)
+
